475e7444277358c792c926df1b98fc051d4e4442
Fransiska <fransiska26@hotmail.com>
2024-03-01 21:46:31 +0100

    implementation FSM

--- state_machine.cpp
+++ state_machine.cpp
@@ -1,5 +1,75 @@
 #include <iostream>
-
+#include <systemc.h>
 #include "state_machine.h"
 
-// TODO
+void stateMachine::process()
+{   curPos++;
+    switch(curState)
+    {
+        case Start:
+            if (input.read() == inG) {
+                curState = G;
+            }
+            else if (input.read() == inA || input.read() == inC || input.read() == inT ){
+                curState = Start;
+            }
+        break;
+        
+        case G:
+            if (input.read() == inA ){
+                curState = GA;
+            }
+            else if (input.read() == inG) {
+                curState = G;
+            }
+            else if (input.read() == inC || input.read() == inT) {
+                curState = Start;
+            }
+        break;
+
+        case GA:
+            if (input.read() == inA ){
+                curState = GAA;
+            }
+            else if (input.read() == inG) {
+                curState = G;
+            }            
+            else if (input.read() == inC || input.read() == inT ){
+                curState = Start;
+            }            
+        break;
+
+        case GAA:
+            if (input.read() == inG ){
+                curState = GAAG;
+            }
+            // Answer for part 3 /GA{2,}+G/:
+            else if (input.read() == inC || input.read() == inT ){
+                curState = Start;
+            }
+            else if (input.read() == inA){
+                curState = GAA;
+            }
+
+            // Answer for part 1 /GAAG/:
+            // else if (input.read() == inA || input.read() == inC || input.read() == inT ){
+            //     curState = Start;
+            // }
+        break;        
+        
+        case GAAG:
+            counter++;
+            cout << "counter: " << counter << "  position: " << curPos-4 << "-" << curPos << endl;            
+            if (input.read() == inG ){
+                curState = G;
+            }
+            else if (input.read() == inA || input.read() == inC || input.read() == inT ){
+                curState = Start;
+            }
+        break;
+
+        default:
+            cout << "invalid input " << endl;
+        break;
+    }
+}
--- state_machine.h
+++ state_machine.h
@@ -4,7 +4,30 @@
 #include <systemc.h>
 
 // TODO
+SC_MODULE(stateMachine)
+{
+    private:
+    enum base {Start, G, GA, GAA, GAAG};
+    void process();
+    base curState = base::Start;
+    const char inG = 'G';
+    const char inA = 'A'; 
+    const char inC = 'C';
+    const char inT = 'T';
+    int counter = 0;
+    int curPos = 0;
 
+    public:
+    sc_in<char> input;
+    sc_in<bool> clk;
+
+    SC_CTOR (stateMachine): input("input"), clk("clk")
+    {
+        SC_METHOD (process);
+        sensitive << clk.pos();
+        dont_initialize();
+    }
+};
 #endif // STATE_MACHINE_H
 
 
